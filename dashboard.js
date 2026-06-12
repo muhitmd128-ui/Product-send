b5b07e3116a14aa3eb60bcbfab14aa6be1b31c2c
muhitmd128-ui <muhitmd128@gmail.com>
2026-06-12 08:22:39 +0600

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -83,7 +83,7 @@ window.completeOrder = function(orderId) {
   // --- NEW: Generate QR Code automatically ---
 // This creates the QR code pointing exactly to your customer menu URL
 new QRCode(document.getElementById("qrcode"), {
-    text: "https://github.com/muhitmd128-ui/Product-send.git/menu.html",
+    text: "https://github.com/muhitmd128-ui/Product-send.io/menu.html",
     width: 150,
     height: 150,
     colorDark : "#000000",
